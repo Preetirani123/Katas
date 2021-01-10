1b6d363ed5af8fa132be8e3a96d17e43d6036659
Preetirani123 <rani.preeti3333@gmail.com>
2021-01-10 22:11:40 +0000

    Kata 1 - Sum the Largest Numbers

---
+++ sum_largest_number.js
@@ -0,0 +1,8 @@
+function sumLargestNumbers(array){
+  let sorted = array.sort(function(a, b){return b - a});
+  return sorted[0] + sorted[1];
+}
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
