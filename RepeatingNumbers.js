a567208435c01797468ebdfb356a281f5d5ab492
Preetirani123 <rani.preeti3333@gmail.com>
2021-01-13 07:25:07 +0000

    repeating numnbers

---
+++ RepeatingNumbers.js
@@ -0,0 +1,17 @@
+const repeatNumbers = function(data) {
+
+  let result = [];
+
+  for (let i = 0; i < data.length; i++){
+    let num = "";
+    for (let j = 0; j < data[i][1]; j++){
+      num = num + data[i][0]
+    }
+    result.push(num);
+  }
+  return result;
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
